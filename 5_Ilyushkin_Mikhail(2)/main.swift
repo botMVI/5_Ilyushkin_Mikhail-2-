64ab193d393bde3baabcbe28affc71e15368b539
Mike <ilmivy1024@gmail.com>
2020-07-30 22:11:03 +0600

    Initial Commit

---
+++ 5_Ilyushkin_Mikhail(2)/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  5_Ilyushkin_Mikhail(2)
+//
+//  Created by Michael Iliouchkin on 30.07.2020.
+//  Copyright © 2020 Michael Iliouchkin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
